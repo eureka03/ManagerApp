2bd44aae4293b86fba7b19aa9a0f91d82118b809
Eureka <eurekaripfumelo@gmail.com>
2026-05-20 23:13:04 +0200

    Making changes to the theme of the app

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,9 @@
-
+import '../Styles/Homepage.css';
 
 export default function Homepage(){
     return(
-        <div>
-            <h1>Welcome to The ManagerApp</h1>
+        <div className="Homepage">
+            <h1 className="Homepage-heading">Welcome to The ManagerApp</h1>
         </div>
     )
 }
