acf569f0654b4b0854b1a8ea8e263c58efb368fb
eureka03 <eurekaripfumelo@gmail.com>
2026-05-29 10:01:28 +0200

    Changes to the homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,16 @@
 import Heading from '../components/Heading.jsx';
+import '../styles/Homepage.css';
 
 export default function Homepage(){
     return(
-        <div>
+        <div className="homepage">
             <Heading/>
+            <div className="homepage-content">
+                <div className="text-container">
+                    <h1 className="homepage-title">Unlock the power <br/>of customer <br/>experiences</h1>
+
+                </div>
+            </div>
         </div>
     )
 }
